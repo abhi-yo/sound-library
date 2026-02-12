a457ba1f3c725a7ea2cb4c1aaa2282dbe7bfc6fa
abhi-yo <akshatsing11@gmail.com>
2026-02-12 21:37:03 +0530

    fix: add missing React component export to usage page

---
+++ app/usage/page.tsx
@@ -0,0 +1,9 @@
+import { UsageGuide } from '@/components/usage-guide';
+
+export default function UsagePage() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <UsageGuide />
+    </div>
+  );
+}
